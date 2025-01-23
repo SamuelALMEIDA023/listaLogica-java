75eebb9aee15b96554f18bb8da96ef7f96c6e796
Samuel Almeida <134805776+SamuelALMEIDA023@users.noreply.github.com>
2025-01-23 10:18:39 -0300

    Update Program.java

--- valoresEspecíficos/src/application/Program.java
+++ valoresEspecíficos/src/application/Program.java
@@ -5,52 +5,52 @@ import java.util.Scanner;
 
 public class Program {
 
-	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		int n;
+    public static void main(String[] args) {
+          Locale.setDefault(Locale.US);
+	  Scanner sc = new Scanner(System.in);
+	  int n;
 		
-		do {
+	  do {
             System.out.print("Digite a quantidade de números (positivo e menor que 20): ");
             n = sc.nextInt();
-        } while (n <= 0 || n >= 20);
+          } while (n <= 0 || n >= 20);
 		
 		
-		int[] valores = new int[n];
-		int soma = 0;
-		int maior = Integer.MIN_VALUE;
-		int menor = Integer.MAX_VALUE;
-		int positivos = 0;
-		int negativos = 0;
+	   int[] valores = new int[n];
+	   int soma = 0;
+	   int maior = Integer.MIN_VALUE;
+	   int menor = Integer.MAX_VALUE;
+           int positivos = 0;
+	   int negativos = 0;
 		
-        for (int i = 0; i < n; i++) {
-            System.out.print("Digite o " + (i + 1) + "º valor: ");
-            valores[i] = sc.nextInt();
-            soma += valores[i];
-            if (valores[i] > maior) {
-            	maior = valores[i];
-            }
-            else if (valores[i] < menor) {
-            	menor = valores[i];
-            }
-            else if (valores[i] > 0) {
-            	positivos++;
-            }
-            else if (valores[i] < 0) {
-            	negativos++;
-            }
-        }
+           for (int i = 0; i < n; i++) {
+               System.out.print("Digite o " + (i + 1) + "º valor: ");
+               valores[i] = sc.nextInt();
+               soma += valores[i];
+               if (valores[i] > maior) {
+            	   maior = valores[i];
+               }
+               else if (valores[i] < menor) {
+            	   menor = valores[i];
+               }
+               else if (valores[i] > 0) {
+            	   positivos++;
+               }
+               else if (valores[i] < 0) {
+            	   negativos++;
+               }
+           }
         
-        double media = (double) soma / n;
-        double porcentagemPositivos = (double) positivos / n * 100;
-        double porcentagemNegativos = (double) negativos / n * 100;
+           double media = (double) soma / n;
+           double porcentagemPositivos = (double) positivos / n * 100;
+           double porcentagemNegativos = (double) negativos / n * 100;
         
-        System.out.println("Maior valor: " + maior);
-        System.out.println("Menor valor: " + menor);
-        System.out.println("Soma dos valores: " + soma);
-        System.out.printf("Media: %.2f", media);
-        System.out.println("Porcentagem de numeros positivos: " + porcentagemPositivos + "%");
-        System.out.println("Porcentagem de numeros negativos: " + porcentagemNegativos + "%");
+           System.out.println("Maior valor: " + maior);
+           System.out.println("Menor valor: " + menor);
+           System.out.println("Soma dos valores: " + soma);
+           System.out.printf("Media: %.2f", media);
+           System.out.println("Porcentagem de numeros positivos: " + porcentagemPositivos + "%");
+           System.out.println("Porcentagem de numeros negativos: " + porcentagemNegativos + "%");
        
 		
 		
